bb8150f3cb39cfa0647b2571207374e22c972425
Benjamin-Knoepfle <borschi@gmail.com>
2024-04-09 13:52:38 +0000

    created model testcases

--- starter/tests/test_model.py
+++ starter/tests/test_model.py
@@ -1,12 +1,11 @@
 import sys
 sys.path.insert(1, 'starter/starter/ml')
-from model import train_model, write_model
+from sklearn.tree import DecisionTreeClassifier
+from model import train_model, write_model, read_model
 import data
 import pandas as pd
 
-def test_model_creation():
-    train = pd.read_csv('starter/tests/test_data/census_test.csv')
-    cat_features = [
+cat_features = [
         "workclass",
         "education",
         "marital-status",
@@ -16,14 +15,37 @@ def test_model_creation():
         "sex",
         "native-country",
     ]
+
+def test_model_creation():
+    train = pd.read_csv('starter/tests/test_data/census_test.csv')
     X_train, y_train, encoder, lb = data.process_data(
         train, categorical_features=cat_features, label="salary", training=True
     )
-    model = train_model(X_train, y_train)
-    assert model != None
+    clf = train_model(X_train, y_train)
+    assert isinstance(clf, DecisionTreeClassifier)
 
-#def test_write_model():
-#    model = None
-#    dest_pth = 'starter/tests/test_data'
-#    write_model(model, dest_pth)
-   
+def test_read_model():
+    src_pth = 'starter/tests/test_data'
+    clf = read_model(src_pth)
+    assert isinstance(clf, DecisionTreeClassifier)
+    
+def test_inference():
+    src_pth = 'starter/tests/test_data'
+    features = pd.read_csv(f'{src_pth}/census_test.csv')
+    features.drop(['salary'], inplace=True, axis=1)
+    encoder, lb = data.read_preprocessors(src_pth)
+    clf = read_model(src_pth) 
+    
+    processed_features = data.process_data(
+        features,
+        categorical_features=cat_features,
+        training=False,
+        encoder=encoder,
+        lb=lb)[0]
+    
+    prediction = clf.predict(processed_features)
+
+    assert len(features)==len(prediction)
+    assert max(prediction)==1
+    assert min(prediction)==0 
+      
